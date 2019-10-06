e8bc6b44131bcb57f86fc9123512362a276c7293
Heber Holanda <heberpcl@gmail.com>
2019-10-06 13:05:54 -0300

    obj and bin remove, gitignore add

---
+++ Core-Project.Repository/Core_ProjectContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace Core_Project.Repository
+{
+    public class Core_ProjectContext : DbContext
+    {
+        public Core_ProjectContext(DbContextOptions<Core_ProjectContext> options) : base (options) {}        
+
+    }
+}
