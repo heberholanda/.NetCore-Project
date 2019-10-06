36f4776d375456a02dd2f07990e21bd948f97bcf
Heber Holanda <heberpcl@gmail.com>
2019-10-06 12:07:13 -0300

    Rename fix

--- Core-Project.API/Program.cs
+++ Core-Project.API/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace CoreProject.API
+namespace Core_Project.API
 {
     public class Program
     {
--- Core-Project.API/Startup.cs
+++ Core-Project.API/Startup.cs
@@ -11,7 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
-namespace CoreProject.API
+namespace Core_Project.API
 {
     public class Startup
     {
