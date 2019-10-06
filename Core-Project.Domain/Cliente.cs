170bc74318b725a1ef7825d598074c7fb5390335
Heber Holanda <heberpcl@gmail.com>
2019-10-06 12:47:42 -0300

    models

---
+++ Core-Project.Domain/Cliente.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Core_Project.Domain
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public int Documento { get; set; }
+        public DateTime? DataCadastro { get; set; }
+        public List<Produto> Produtos { get; set; }
+    }
+}
---
+++ Core-Project.Domain/Produto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Core_Project.Domain
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public decimal Valor { get; set; }
+        public int ProdutoDetalhesId { get; set; }
+        public ProdutoDetalhe ProdutoDetalhe { get; set; }
+    }
+}
